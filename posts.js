3474af7c500e05967147ac2b3f27b6ef7e00e6f6
nauchoprieto <nahuelprieto826@gmail.com>
2025-07-15 16:56:25 -0300

    Update posts.js

--- posts.js
+++ posts.js
@@ -1,7 +1,9 @@
 const posts = [
   {
-    titulo: "El fútbol no se detiene: lecciones de un amistoso inolvidable",
-    fecha: "12 de abril de 2025",
-    resumen: "En un amistoso con Parque del Plata en 2024, vi algo que me marcó para siempre. Un compañero, titular en mi puesto, se rompió la rodilla tras un choque fuerte. Lo vi tirado, llorando, y lo que más me impactó fue la frialdad del momento. Lo ayudaron, sí, pero nadie lo contuvo como debía ser. Lo levantó una ambulancia, y el partido siguió como si nada. Me tocó entrar después, y no era yo. Estaba cagado, no por miedo a lesionarme, sino por respeto. Él estaba en un hospital, y nosotros jugando un amistoso de mierda. Sentí que debimos haber parado.Ese día entendí que el fútbol no tiene compasión. No espera, no frena. Mientras alguien sufre, la pelota sigue rodando. Como en la vida.",
+    titulo: "Recordatorio para el alma",
+    fecha: "15 de mayo de 2025",
+    resumen: "“No soy débil por hacer las cosas a mi ritmo. Estoy cuidando mi proceso y eso es un acto de valentía. Cada paso pequeño es un avance real, no una derrota. Mi fuerza está en reconocer lo que necesito para seguir adelante.”
+
+",
   }
 ];
